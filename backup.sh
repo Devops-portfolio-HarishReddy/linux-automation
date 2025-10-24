aa98a8f7a0506c1198f5fa61ca0e81f0fafbaca9
iHarishReddy <harish.manikantareddy@gmail.com>
2025-10-24 16:01:24 +0530

    Add logging function to backup script

--- backup.sh
+++ backup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+log() {
+  echo "[$(date '+%F %T')] $1" | tee -a /var/log/toolkit.log
+}
+
+
 usage() {
   echo "Usage: $0 [source_dir] [backup_dir]"
   echo "Example: $0 /home/ubuntu/data /home/ubuntu/backups"
@@ -18,6 +23,6 @@ DATE=$(date +%F)
 mkdir -p "$BACKUP_DIR"
 tar -czf "$BACKUP_DIR/backup_$DATE.tar.gz" "$SRC_DIR"
 find "$BACKUP_DIR" -type f -mtime +7 -delete
-echo "Backup completed successfully."
+log "Backup completed successfully."
 
 
